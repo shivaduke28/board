c83a4088adb3ce047cb192f09905fd7a3329352b
shivaduke28 <shivaduke28@gmail.com>
2025-04-16 00:41:57 +0900

    initial commit

---
+++ Board/BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardApp.swift
+//  Board
+//
+//  Created by Kei Hasegawa on 2025/04/16.
+//
+
+import SwiftUI
+
+@main
+struct BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoardTests/BoardTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoardTests.swift
+//  BoardTests
+//
+//  Created by Kei Hasegawa on 2025/04/16.
+//
+
+import Testing
+@testable import Board
+
+struct BoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
