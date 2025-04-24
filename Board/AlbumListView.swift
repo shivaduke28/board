730a77cc0b4e5e1b87a23b25d97428df19abc95a
shivaduke28 <shivaduke28@gmail.com>
2025-04-24 19:42:20 +0900

    album list view

--- Board/AlbumListView.swift
+++ Board/AlbumListView.swift
@@ -7,19 +7,59 @@ struct AlbumListView: View {
     var body: some View {
         VStack {
             HStack {
-                Text("Album").fontWeight(.bold).frame(maxWidth: .infinity, alignment: .leading)
+                Text("Album").fontWeight(.bold).frame(
+                    maxWidth: .infinity,
+                    alignment: .leading
+                )
+                Text("Artist").fontWeight(.bold).frame(
+                    maxWidth: .infinity,
+                    alignment: .leading
+                )
+                Text("Artists").fontWeight(.bold).frame(
+                    maxWidth: .infinity,
+                    alignment: .leading
+                )
+                Text("Year").fontWeight(.bold).frame(
+                    maxWidth: .infinity,
+                    alignment: .leading
+                )
             }
             .padding(.horizontal, 12)
             Divider()
             List {
-                ForEach(srfLibrary.albums.sorted(by: {$0.value.metadata.title < $1.value.metadata.title}), id: \.value.id) { _, album in
+                ForEach(
+                    srfLibrary.albums.sorted(by: {
+                        $0.value.metadata.title < $1.value.metadata.title
+                    }),
+                    id: \.value.id
+                ) { _, album in
                     HStack {
-                        Button(album.metadata.title) {}
-                        Spacer()
+                        Text(album.metadata.title).frame(
+                            maxWidth: .infinity,
+                            alignment: .leading
+                        )
+                        Text(album.metadata.artist).frame(
+                            maxWidth: .infinity,
+                            alignment: .leading
+                        )
+                        HStack {
+                            ForEach(album.metadata.artists, id: \.self) {
+                                artist in
+                                Button(artist) {}
+                            }
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                        Text(toString(album.metadata.year)).frame(
+                            maxWidth: .infinity,
+                            alignment: .leading
+                        )
                     }
                 }
             }
             .listStyle(.plain)
         }
+    }
+
+    func toString(_ num: Int?) -> String {
+        num.map(String.init) ?? ""
     }
 }
